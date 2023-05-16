505198b43d4054ef4a7a9195884209116dae293f
unknown <joao.silva@germinare.org.br>
2023-05-16 17:02:28 -0300

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("sout :o");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe seu perfil: ");
+        System.out.println("Seu perfil é: " + sc.nextLine());
     }
 }
